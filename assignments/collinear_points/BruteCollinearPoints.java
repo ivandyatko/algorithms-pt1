104fca6d6e43127a9d48f29ae54dc756fc806947
Ivan Dyatko <dyatkoivan@gmail.com>
2016-09-14 09:46:58 +0300

    LineSegment and BruteCollinearPoints added

---
+++ assignments/collinear_points/BruteCollinearPoints.java
@@ -0,0 +1,21 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+
+    }    // finds all line segments containing 4 points
+
+
+    /**
+     * The number of line segments.
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+
+    /**
+     * The line segments.
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
